48948d7d424af39877562eee547472e63eafe31a
adroi3 <hedtke36@gmail.com>
2018-04-15 22:03:40 +0200

    Added confirmation for the name slot

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const EXPRESS = require("express");
-const ALEXA = require("alexa-app");
-
-const EXPRESSAPP = EXPRESS();
+let express = require("express");
+let alexa = require("alexa-app");
+let expressApp = express();
 
 const APP_PATH = 'helloworldmanager';
-const APP = new ALEXA.app(APP_PATH);
+
+let app = new alexa.app(APP_PATH);
 
 const PORT = 8080;
 
 // setup the alexa app and attach it to express before anything else
-APP.express({
-    expressApp: EXPRESSAPP,
+app.express({
+    expressApp: expressApp,
 
     // verifies requests come from amazon alexa. Must be enabled for production.
     // You can disable this if you're running a dev environment and want to POST
@@ -22,20 +22,31 @@ APP.express({
     debug: true
 });
 
-APP.launch(function (request, response) {
+app.launch(function (request, response) {
     response.say("Wilkommen");
 });
 
-APP.intent("greeting", {
+app.intent("greeting", {
     "slots": { "name": "AMAZON.DE_FIRST_NAME" },
     "utterances": ["sage hallo zu {-|name}"]
 },
     function (request, response) {
-        var name = request.slot("name");
+        let name = request.slot("name");
+        
+        request.getSession().set("name", name);
+
+        response.say(`wollen sie ${name} hallo sagen?`).shouldEndSession(false);
+    }
+);
+
+app.intent("AMAZON.YesIntent", null,
+    function (request, response) {
+        let name = request.getSession().get("name");
+
         response.say(`Hallo ${name}`);
     }
 );
 
-EXPRESSAPP.listen(PORT);
+expressApp.listen(PORT);
 
 console.log("Listening on port " + PORT + ", try http://localhost:" + PORT + `/${APP_PATH}`);
